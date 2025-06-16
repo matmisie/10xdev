a8656a335603d9c8c088be704f3f6680139273d6
Mateusz Misiewicz <mateusz.misiewicz@gmail.com>
2025-06-16 23:22:48 +0200

    fix CF

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
       },
     },
     resolve: {
-      alias: {
+      alias: import.meta.env.PROD && {
         "react-dom/server": "react-dom/server.edge",
       },
     },
