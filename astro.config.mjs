52696630a556164fdfa796d48b4307d9b49f24e3
Mateusz Misiewicz <mateusz.misiewicz@gmail.com>
2025-06-16 16:53:07 +0200

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
-import istanbul from "vite-plugin-istanbul";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,18 +12,7 @@ export default defineConfig({
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
-    plugins: [
-      tailwindcss(),
-      process.env.MODE === "test"
-        ? istanbul({
-            include: "src/*",
-            exclude: ["node_modules", "src/test/"],
-            extension: [".js", ".ts", ".tsx", ".astro"],
-            requireEnv: true,
-            nycrcPath: "./.nycrc.json",
-          })
-        : null,
-    ],
+    plugins: [tailwindcss()],
     server: {
       watch: {
         ignored: ["**/.idea/**", "**/tests/**", "**/.git/**"],
